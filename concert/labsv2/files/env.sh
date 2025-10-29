d6adbd122313a2c0762a02c89e7736b94a9b655f
John Abbott <jabbott@us.ibm.com>
2025-10-29 13:31:56 -0600

    Deployed 95e9de4 with MkDocs version: 1.6.1

---
+++ concert/labsv2/files/env.sh
@@ -0,0 +1,6 @@
+export INSTALL_DIR=/mnt/concert/ibm-concert
+
+export DOCKER_EXE=podman
+export IBM_REGISTRY=cp.icr.io/cp
+export IBM_REGISTRY_USER=cp
+export IBM_REGISTRY_PASSWORD="YOUR_ENTITLEMENT_KEY"
